e4f3025388d86a175ea2a0ae39c89e481a1207ae
unknown <imanol.mb@hotmail.com>
2025-11-08 17:44:56 -0300

    limpieza

--- epic-rocket-clicker-main/src/index.js
+++ epic-rocket-clicker-main/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Game from './components/Game/game.js';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
